d8a7aed7e364553b85f6f2a5888b79e901a138af
chams1517 <141412873+chams1517@users.noreply.github.com>
2026-03-05 23:45:09 +0100

    Update environment.ts

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,7 +1,4 @@
-export const environment :
-{
-  production?:boolean,
-}
-= {
-    production: false,
-  };
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8081'
+};
